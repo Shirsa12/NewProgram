fdcee54594c3ef5784dff51ad3070518c664c19d
Shirsa <Shirsa@DESKTOP-TFKF9ER>
2023-08-19 11:31:09 +0530

    POLYMORPHISM

---
+++ src/Numbers/MultipleNumber.java
@@ -0,0 +1,32 @@
+package Numbers;
+
+public class MultipleNumber{ 
+	 public static void createcontact( String name, int num){
+
+	System.out.println(name);
+	System.out.println(num);
+	}
+	public static void createcontact(String name, int num1, int num2)
+	{
+	System.out.println(name);
+	System.out.println(num1);
+	System.out.println(num2);
+	}
+	public static void createcontact (String name, int num1, int num2, int num3)
+	{
+	System.out.println(name);
+	System.out.println(num1);
+	System.out.println(num2);
+	System.out.println(num3);
+	}
+	public static void main(String[] args)
+	{ 
+	createcontact("Shirsa", 98054);
+
+	createcontact("Shreya", 20897, 12345); 
+
+	createcontact("Isha", 12561, 26622, 234545); 
+	}
+
+	}
+
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JAVAProgrampractice {
+}
